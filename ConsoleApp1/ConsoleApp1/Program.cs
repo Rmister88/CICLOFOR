3631ad8fa8600532302a64c2cfdfa41c98c26d59
Hugo Alejandro Ramos Garcia <hramos0@ucol.mx>
2019-08-24 18:03:04 -0500

    CICLOS

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ciclo_For_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Imprime los numeros del X hasta Y para comenzar la numeracion");
+            Console.WriteLine("De donde arrancamos el ciclo (X): ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De donde terminamos el ciclo (Y): ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto deseas que avance el programa: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i = x; i > y; i -= z)
+
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
+            else
+            {
+                for (int i = x; i < y; i += z)
+
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
+        }
+    }
+}
